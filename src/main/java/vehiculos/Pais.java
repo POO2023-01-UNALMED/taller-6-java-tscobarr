c93e086ebca7009cef7eb257258c923fecd9ff29
tscobarr <124937392+tscobarr@users.noreply.github.com>
2023-03-26 15:55:13 -0500

    Test5

--- src/main/java/vehiculos/Pais.java
+++ src/main/java/vehiculos/Pais.java
@@ -1,12 +1,30 @@
 package vehiculos;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Pais {
 	private String nombre;
+	static Map<Pais, Integer> ventasPorPais = new HashMap <>();
 	
 	public Pais(String nombre) {
 		this.nombre = nombre;
 	}
 	
+	public static Pais paisMasVendedor() {
+		int max = -1;
+		Pais paisMasVendedor = null;
+		
+		for(Map.Entry<Pais, Integer> entry: ventasPorPais.entrySet()) {
+			if (entry.getValue() >  max) {
+				max = entry.getValue();
+				paisMasVendedor = entry.getKey();
+			}
+		}
+		
+		return paisMasVendedor;
+	}
+		
 	public String getNombre() {
 		return nombre;
 	}
